d159e8c7f05245736a37d9ddd508fe668d90922a
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-14 00:45:24 +0900

    feat: email urls

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from accounts.views.email import EmailView
 from accounts.views.signup import SignupView
 
 urlpatterns = [
     path("/signup", SignupView.as_view()),
+    path("/email", EmailView.as_view()),
 ]
