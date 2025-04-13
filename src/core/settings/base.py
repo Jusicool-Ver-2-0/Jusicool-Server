22d32b9904b8085beda0620013b934f36eeb3d67
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-13 20:19:36 +0900

    feat: exceptions setting

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     "accounts"
 ]
 
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "core.exceptions.custom_exception_handler"
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
