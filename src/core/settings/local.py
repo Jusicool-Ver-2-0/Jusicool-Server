0f1e7178a7fcd23e35fabac79c8a3a6ff60a8428
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-13 13:46:16 +0900

    update: default cache setting

--- src/core/settings/local.py
+++ src/core/settings/local.py
@@ -13,3 +13,10 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379"),
+    }
+}
