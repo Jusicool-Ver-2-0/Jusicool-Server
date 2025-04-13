3ba5b281d6137b34479b0b8d72c5582eb82d4249
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-14 00:43:22 +0900

    feat: email view

---
+++ src/accounts/views/email.py
@@ -0,0 +1,24 @@
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from accounts.serializers import EmailRequestSerializer, EmailValidateSerializer
+from accounts.services.email import MailService
+from core.authentications import CsrfExemptSessionAuthentication
+
+
+class EmailView(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication, )
+
+    def get(self, request: Request) -> Response:
+        serializer = EmailRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        MailService().request(serializer)
+        return Response(status=status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        serializer = EmailValidateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        MailService().validate(request=request, serializer=serializer)
+        return Response(status=status.HTTP_200_OK)
