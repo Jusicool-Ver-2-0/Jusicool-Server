07455ed85f1ced389d9c3c3fecffc2c45a4e7e9c
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-14 00:45:18 +0900

    feat: email exception

--- src/accounts/exceptions.py
+++ src/accounts/exceptions.py
@@ -6,3 +6,9 @@ class AccountAlreadyExistException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Account already exist'
     default_code = 'account_already_exist'
+
+
+class CodeIsNotValidException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Code is not valid'
+    default_code = 'code_is_not_valid'
