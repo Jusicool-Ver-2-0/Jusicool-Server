934c621ecf30bb7a6d86d0d801d9a4f8415caa3d
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-14 01:23:30 +0900

    update: email validate url, view

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from accounts.views.email import EmailView
+from accounts.views.email import EmailRequestView, EmailValidateView
 from accounts.views.signin import SigninView
 from accounts.views.signup import SignupView
 
 urlpatterns = [
     path("/signup", SignupView.as_view()),
     path("/signin", SigninView.as_view()),
-    path("/email", EmailView.as_view())
+    path("/email/send", EmailRequestView.as_view()),
+    path("/email/verify", EmailValidateView.as_view()),
 ]
--- src/accounts/views/email.py
+++ src/accounts/views/email.py
@@ -8,17 +8,23 @@ from accounts.services.email import EmailService
 from core.authentications import CsrfExemptSessionAuthentication
 
 
-class EmailView(APIView):
+class EmailRequestView(APIView):
     authentication_classes = (CsrfExemptSessionAuthentication, )
 
-    def get(self, request: Request) -> Response:
+    def post(self, request: Request) -> Response:
         serializer = EmailRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         EmailService().request(serializer)
         return Response(status=status.HTTP_200_OK)
+
+
+class EmailValidateView(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication, )
 
     def post(self, request: Request) -> Response:
         serializer = EmailValidateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         EmailService().validate(request=request, serializer=serializer)
         return Response(status=status.HTTP_200_OK)
+
+
