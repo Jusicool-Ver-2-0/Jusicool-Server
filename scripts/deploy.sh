32423e752c584ee3cd6d6c279696f58b759e02a7
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-23 20:04:49 +0900

    update: project dir

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 
 if [[ "$DEPLOYMENT_GROUP_NAME" == "stage" ]]; then
-  /ubuntu/home/stage/Jusicool-Server/scripts/stage.sh
+  /home/ubuntu/stage/Jusicool-Server/scripts/stage.sh
 elif [[ "$DEPLOYMENT_GROUP_NAME" == "prod" ]]
-  /ubuntu/home/prod/Jusicool-Server/scripts/prod.sh
+  /home/ubuntu/prod/Jusicool-Server/scripts/prod.sh
 fi
