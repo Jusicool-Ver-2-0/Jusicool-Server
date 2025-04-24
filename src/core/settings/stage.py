a5a2d6fbc0dca1ac63349e033f21dc163f9febd4
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-24 17:27:25 +0900

    feat: stage settings

---
+++ src/core/settings/stage.py
@@ -0,0 +1,30 @@
+from .base import *
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    os.environ.get("ALLOWED_HOSTS").split(","),
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "jusicool"),
+        "USER": os.environ.get("DB_USER", "jusicool-postgres-admin"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "jusicool-postgres-admin-password"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379"),
+    }
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
