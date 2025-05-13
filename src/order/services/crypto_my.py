c6cc7775009c8a4adf9e4b7f02b10286743eec0f
Jueuunn7 <wndmsrla1234@gmail.com>
2025-05-13 12:29:07 +0900

    feat: query my order api

--- src/order/services/crypto_my.py
+++ src/order/services/crypto_my.py
@@ -1,14 +1,28 @@
+from holding.models import Holding
 from order.enums import ReserveType, OrderStatus
 from order.models import Order
 
 
 class CryptoMyOrderService:
-    def __init__(self, order: Order = Order):
+    def __init__(
+            self,
+            order: Order = Order,
+    ):
         self.order = order
 
-    def get(self, user):
-        return self.order.objects.filter(
-            user=user,
-            reserve_type=ReserveType.RESERVE,
-            status=OrderStatus.PENDING
-        ).prefetch_related("market")
+    def get(self, user, order_type):
+        if order_type == "reserve":
+            return self.order.objects.filter(
+                user=user,
+                reserve_type=ReserveType.RESERVE,
+                status=OrderStatus.PENDING
+            ).prefetch_related("market")
+        elif order_type == "completed":
+            return self.order.objects.filter(
+                user=user,
+                status=OrderStatus.COMPLETED
+            ).prefetch_related("market")
+        else:
+            return self.order.objects.filter(
+                user=user,
+            ).prefetch_related("market")
