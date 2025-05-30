9990dfe8e4e2c4b9335148900c0e58094c5bafe3
Jueuunn7 <wndmsrla1234@gmail.com>
2025-05-30 10:13:09 +0900

    feat: crypto list response cache

--- src/market/services/list.py
+++ src/market/services/list.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+
 from market.models import Market
 
 
@@ -6,6 +8,13 @@ class MarketListService:
         self.market = market
 
     def get_list(self, market_type: str = None):
+        return cache.get_or_set(
+            f"market_list_{market_type or 'ALL'}",
+            lambda: self._fetch_from_db(market_type),
+            timeout=60 * 60 * 24
+        )
+
+    def _fetch_from_db(self, market_type: str = None):
         if market_type:
             return self.market.objects.filter(market_type=market_type)
         return self.market.objects.all()
