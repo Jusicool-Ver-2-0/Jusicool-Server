019a623b7cea471cf5d7d7dd04bd937d7354af33
Jueuunn7 <wndmsrla1234@gmail.com>
2025-06-11 13:47:44 +0900

    feat: market update command

---
+++ src/market/management/commands/update_crypto.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from market.tasks.crypto.update import update_crypto_task
+
+class Command(BaseCommand):
+    help = 'Run update_crypto_task Celery task'
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.NOTICE("Sending update_crypto_task to Celery..."))
+        result = update_crypto_task.delay()
+        self.stdout.write(self.style.SUCCESS(f"Task sent. Task ID: {result.id}"))
