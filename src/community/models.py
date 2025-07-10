c7b03134b31c898e2a498bf9d39c49b764afeb45
Jueuunn7 <wndmsrla1234@gmail.com>
2025-07-10 17:23:19 +0900

    update: board pk

--- src/community/models.py
+++ src/community/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.db.models import QuerySet
 
+from market.models import Market
 from user.models import User
 from core.models import BaseModel
 
 
 class Board(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    market = models.ForeignKey(Market, on_delete=models.CASCADE)
+
     title = models.CharField(max_length=50)
     content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     comment: "QuerySet[Comment]"
 
@@ -17,11 +20,13 @@ class Board(BaseModel):
 
 
 class Comment(BaseModel):
-    comment = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(
         Board, on_delete=models.CASCADE, related_name="comment"
     )
+
+    comment = models.TextField()
+
 
     class Meta:
         db_table = "comment"
