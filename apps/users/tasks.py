8857359e1cc0c305bcd87b0a3aac2e2cdcac5aab
Jueuunn7 <wndmsrla1234@gmail.com>
2025-12-14 01:41:58 +0900

    refactor: email value

--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -10,12 +10,12 @@ from core.cache.prefix import CacheKeyPrefix
 
 
 @shared_task()
-def send_email_verify_code(email: str) -> None:
+def send_email_verify_code(user_email: str) -> None:
     code = random.randint(100000, 999999)
 
     Cache.set(
         prefix=CacheKeyPrefix.email_verification_code,
-        key=email,
+        key=user_email,
         value=code,
         timeout=60 * 10,
     )
@@ -23,7 +23,7 @@ def send_email_verify_code(email: str) -> None:
     html_content = render_to_string(
         template_name="verify_code.html",
         context={
-            "recipient_name": email,
+            "recipient_name": user_email,
             "verification_code": code,
         },
     )
@@ -31,7 +31,7 @@ def send_email_verify_code(email: str) -> None:
     email = EmailMultiAlternatives(
         subject=f"Jusicool 메일 인증 코드",
         body=html_content,
-        to=[email],
+        to=[user_email],
         from_email=settings.EMAIL_HOST_USER,
     )
     email.attach_alternative(
