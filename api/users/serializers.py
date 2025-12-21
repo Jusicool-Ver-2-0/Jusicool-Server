8f37af0cdefaccb73ab3f888e1c66fa0f3782866
Jueuunn7 <wndmsrla1234@gmail.com>
2025-12-21 15:16:39 +0900

    refactor: authentication email

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 
 class SendVerifyCodeSerializer(serializers.Serializer):
+    """이메일 인증 요청"""
+
     email = serializers.EmailField(
         help_text="이메일 주소",
     )
